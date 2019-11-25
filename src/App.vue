54069ccf0923f5233fefa2cac0f088d87377b03b
doori30 <rublju0070@naver.com>
2019-11-25 10:14:42 +0900

    pug와 bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
-<template>
-	<div id="app">
-	<div class="container">
-		<div class="jumbotron">
-			<h1>{{title}}</h1>
-			<!--  app이라는 객체의 변수가 됨. -->
-		</div>
-		<!-- <div class="search-wrap"> -->
-		<search-bar 
-		v-bind:value="searchBarQuery" 
-		v-on:@submit="onSubmit"
-		v-on:@reset="onSearchReset">
-		</search-bar>
-
-		<prd-result 
-		v-bind:query="searchBarQuery"></prd-result>
-		<!-- </div> -->
-		
-		</div>
-</div>
+<template lang="pug">
+#app
+	.container
+		.jumbotron
+			h1 {{title}}
+		search-bar(v-bind:value="searchBarQuery" v-on:@submit="onSubmit" v-on:@reset="onSearchReset")
+		prd-result(v-bind:query="searchBarQuery")
+		div
+			b-button(v-b-modal.modal-1="") Launch demo modal
+			b-modal#modal-1(title="BootstrapVue")
+				p.my-4 Hello from modal!
 </template>
 
 <script>
@@ -33,6 +24,7 @@ export default {
 	data() {//전역변수 app안에서 사용하는 데이터
 		return {
 			title: "Vue를 배워봅시다.",
+			modalTitle:"모달창",
 			searchBarQuery:'',
 		}
 	},
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* bootstrap-vue */
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.min.css' 
+// 실제 부트스트랩
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// 부트스트랩이 사용할 수 있도록 해줌
+/* node모듈에서 (정적인)파일을 가져올 때 */
+
 Vue.config.productionTip = false
 
 new Vue({
